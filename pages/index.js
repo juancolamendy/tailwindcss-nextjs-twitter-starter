f8ac81837c23435cba64c71f3b2c3d7c27690d6f
Juan Carlos Olamendy <juan.olamendy@bluvision.com>
2022-04-15 14:31:36 -0400

    setting up twitter auth

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,16 @@
+import { useSession, signIn, signOut } from 'next-auth/react';
+
 export default function Home() {
-  return (
-    <div className="flex flex-row">
-      Hello world!
-    </div>
-  )
+  const { data: session } = useSession()
+
+  if(session) {
+    return <>
+      Signed in as {session.user.email} <br/>
+      <button onClick={() => signOut()}>Sign out</button>
+    </>
+  }
+  return <>
+    Not signed in <br/>
+    <button onClick={() => signIn()}>Sign in</button>
+  </>  
 }
